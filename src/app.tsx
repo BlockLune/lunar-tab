a7d397662eac07b15e1a01ccdc3153bb2fb6564a
BlockLune <39331194+BlockLune@users.noreply.github.com>
2026-03-16 21:23:20 +0800

    feat: support search param `q`

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { SearchBox } from "@/components/search-box";
 import { Settings } from "@/components/settings";
+import { useSettings } from "@/context/settings-context";
+import { performSearch } from "@/utils/search";
 
 function App() {
+	const [searchParams] = useSearchParams();
+	const { settings } = useSettings();
+
+	useEffect(() => {
+		const query = searchParams.get("q");
+		if (query) {
+			performSearch(query, {
+				primaryEngine: settings.primaryEngine,
+				fallbackEngine: settings.fallbackEngine,
+				checkTimeout: settings.checkTimeout,
+			});
+		}
+	}, []);
+
 	return (
 		<div className="min-h-screen flex flex-col items-center justify-center p-5">
 			<Settings />
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "@/index.css";
 import App from "@/app";
 import { SettingsProvider } from "@/context/settings-context";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<SettingsProvider>
-			<App />
-		</SettingsProvider>
+		<BrowserRouter>
+			<SettingsProvider>
+				<App />
+			</SettingsProvider>
+		</BrowserRouter>
 	</StrictMode>,
 );
